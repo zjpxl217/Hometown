152eb2d1a8c90509792f8a6950732dc41f57c3b8
JP Zou <zjpxl217@163.com>
2017-04-23 10:58:23 +0800

    Initial Commit

---
+++ Hometown/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hometown
+//
+//  Created by ZouJP on 17/4/23.
+//  Copyright © 2017年 ZouJP. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
